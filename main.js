b128c10025dfc8aca83af9a9b1e8184a968c19ab
Brittany Mangum <brittanyturner8@gmail.com>
2022-06-10 15:50:04 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const quarterValue = (originalNumber) => {
+    const oneFourth = originalNumber /4
+    return oneFourth
+}
+
+// Invoke the function and store the return value
+const newNumber = quarterValue(16)
+
+// Log the return value
+console.log(newNumber)
+
+// It will work very similarly to the findHalf() function shown above. What you need to do is complete the following tasks:
+
+// Define a parameter that will hold the value of the original number.
+// Divide that number by 4
+// Return the quartered number
+// Store the returned number in a variable
+// Plug that variable into the parenthesis for the console.log()
+// When you run the code, you should see the answer in the console.
